032fca537ad29fb5b457bc2c7f2d0ef5890b2d19
Joel Yujra <yhjoel0@gmail.com>
2026-03-12 01:53:49 -0400

    Resuelve problema operadores relacionales

---
+++ Prog1018.java
@@ -0,0 +1,28 @@
+package ejercicio_1018;
+
+import java.util.Scanner;
+
+public class Prog1018 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt(); 
+		
+		for(int i=0; i<t; i++)
+		{
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			if(a > b)
+				System.out.println(">");
+			else if(a < b)
+				System.out.println("<");
+			else
+				System.out.println("=");
+		}
+
+	}
+
+}
